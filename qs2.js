2aa3030ab2bbf590a872b29e61b10b18788f6e0e
amna <amnaa3631@gmail.com>
2023-02-01 15:57:49 +0300

    update code

--- qs2.js
+++ qs2.js
@@ -1,2 +1,9 @@
-//	Write an arrow function named arrayAverage that accepts an array of numbers and returns
+/*	Write an arrow function named arrayAverage that accepts an array of numbers and returns
 // the average of those numbers.
+*/
+const arrayAverage  = numbers =>{
+	const sum = numbers.reduce((a,b)=> a+b);
+	return sum/numbers.length;
+};
+
+console.log(arrayAverage([1,2,3,4,5,6,7,8]));
